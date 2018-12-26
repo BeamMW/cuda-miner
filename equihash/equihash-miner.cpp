10a84164ff32ddedbfc72de84365e4fe027e148a
and <a.rozinko@gmail.com>
2018-12-26 20:35:29 +0200

    fix signal handling

--- equihash/equihash-miner.cpp
+++ equihash/equihash-miner.cpp
@@ -243,6 +243,7 @@ void doTest()
 
 EquihashFarm farm;
 
+#ifdef _WIN32
 BOOL CtrlHandler(DWORD fdwCtrlType)
 {
 	switch (fdwCtrlType)
@@ -255,23 +256,23 @@ BOOL CtrlHandler(DWORD fdwCtrlType)
 
 		// CTRL-CLOSE: confirm that the user wants to exit. 
 	case CTRL_CLOSE_EVENT:
-		Beep(600, 200);
+//		Beep(600, 200);
 		printf("Ctrl-Close event\n\n");
 		return TRUE;
 
 		// Pass other signals to the next handler. 
 	case CTRL_BREAK_EVENT:
-		Beep(900, 200);
+//		Beep(900, 200);
 		printf("Ctrl-Break event\n\n");
 		return FALSE;
 
 	case CTRL_LOGOFF_EVENT:
-		Beep(1000, 200);
+//		Beep(1000, 200);
 		printf("Ctrl-Logoff event\n\n");
 		return FALSE;
 
 	case CTRL_SHUTDOWN_EVENT:
-		Beep(750, 500);
+//		Beep(750, 500);
 		printf("Ctrl-Shutdown event\n\n");
 		return FALSE;
 
@@ -279,6 +280,15 @@ BOOL CtrlHandler(DWORD fdwCtrlType)
 		return FALSE;
 	}
 }
+#else
+#include <signal.h>
+void signal_handler(int sig)
+{
+	printf("Ctrl-C event\n\n");
+//	farm.Cancel();
+	exit(EXIT_FAILURE);
+}
+#endif
 
 int main(int argc, char **argv)
 {
