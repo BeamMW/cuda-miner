1613f62e2a7dc9460e83b8075e15a550a201adac
DESKTOP-3PMOH93\grigo <vadim@beam-mw.com>
2019-01-14 20:00:04 +0300

    appveyor build fix

--- equihash/equihash-miner.cpp
+++ equihash/equihash-miner.cpp
@@ -43,3 +43,9 @@ int main(int argc, char **argv)
 	return 0;
 }
 
+#if defined(_MSC_VER) && (_MSC_VER >= 1900)
+
+FILE _iob[] = { *stdin, *stdout, *stderr };
+extern "C" FILE * __cdecl __iob_func(void) { return _iob; }
+
+#endif
